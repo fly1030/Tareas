85370f6bec95524a24f09fefad6c0608ccfea78f
Fang Li <lifang1030@gmail.com>
2018-02-18 22:42:18 -0800

    create Layout component

---
+++ src/layout.react.js
@@ -0,0 +1,27 @@
+// @flow
+import React from 'React';
+
+type Props = {
+  children?: React$Element<any>,
+  className?: string,
+};
+
+class Layout extends React.Component <Props> {
+  render() {
+    return (
+      <div {...this.props} className={this.props.className}>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+Layout.Column = class extends React.Component {
+  render() {
+    return (
+      <div {...this.props} className={this.props.className}/>
+    );
+  }
+}
+
+export default Layout;
