a503e93f0702585151f8cd5d869c2fd877145745
Fang Li <lifang1030@gmail.com>
2018-02-21 18:14:29 -0800

    Create dummy test tasks, and render base on selection

--- src/tareasList.react.js
+++ src/tareasList.react.js
@@ -6,14 +6,107 @@ import type keyType from './tareasLeftBar.react';
 
 type Props = {'selectedKey': keyType};
 
+type Tareas = {
+  id: string,
+  title: string,
+  status: "open" | "closed",
+  owner: string,
+  creator: string,
+  subscribers: Array<string>,
+  description: string,
+  priority: "high" | "medium" | "low" | "wishlist",
+};
+
+const currUser = "tester1";
+const tasks = [
+  {
+    id: "000000",
+    title: "test task 0",
+    status: "open",
+    owner: "tester1",
+    creator: "tester1",
+    subscribers: ["tester1", "tester2"],
+    description: "test example 0",
+    priority: "high",
+  },
+  {
+    id: "000001",
+    title: "test task 1",
+    status: "open",
+    owner: "tester1",
+    creator: "tester2",
+    subscribers: ["tester1"],
+    description: "test example 1",
+    priority: "medium",
+  },
+  {
+    id: "000002",
+    title: "test task 2",
+    status: "open",
+    owner: "tester2",
+    creator: "tester1",
+    subscribers: ["tester2"],
+    description: "test example 2",
+    priority: "low",
+  },
+  {
+    id: "000003",
+    title: "test task 3",
+    status: "open",
+    owner: "tester2",
+    creator: "tester1",
+    subscribers: ["tester1", "tester2"],
+    description: "test example 3",
+    priority: "low",
+  },
+];
+
 // TODO: Once backend available, fetch all tasks async base on tab selected
 class tareasList extends React.Component <Props> {
   render() {
+    const tasksList = this._filterTasks();
     return (
       <div className="tareasList">
-        {this.props.selectedKey}
+        {this._renderTareasList(tasksList)}
       </div>
     );
+  }
+
+  _renderTareasList(tasksList: Array<Tareas>) {
+    return (
+      <div>
+        {tasksList.map(task => {
+          return (
+            <div>
+              <span>`T${task.id}`</span>
+              <span>{task.owner}</span>
+              <span>{task.title}</span>
+              <span>{task.status}</span>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+
+  _filterTasks(): Array<Tareas> {
+    let filteredTasks = [];
+    tasks.map(tareas => {
+      if (this.props.selectedKey === 'Owner') {
+        if (tareas.owner === currUser) {
+          filteredTasks.push(tareas);
+        }
+      } else if (this.props.selectedKey === 'Created') {
+        if (tareas.creator === currUser) {
+          filteredTasks.push(tareas);
+        }
+      } else {
+        if (tareas.subscribers.includes(currUser)) {
+          filteredTasks.push(tareas);
+        }
+      }
+    });
+    return filteredTasks;
   }
 
 }
